b1806e70f218a207a6916e30c3eadae216618ad7
RomarioBispo <romario.bsp.silva@gmail.com>
2020-05-04 19:13:41 -0300

    add: configuracao inicial do projeto

---
+++ FONTES/manga/common/src/test/java/br/com/codevelopment/manga/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codevelopment.manga.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
